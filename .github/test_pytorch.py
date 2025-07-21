cebbd9c8b64af541459b1b44f790a849c30c8d31
Andrey885 <asshilov@yandex.com>
2025-07-21 17:58:51 +0300

    fix dim

--- .github/test_pytorch.py
+++ .github/test_pytorch.py
@@ -11,7 +11,7 @@ import npucloud_client
 class SimpleModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv = torch.nn.Conv2d(1, 1, 1, bias=False)
+        self.conv = torch.nn.Conv2d(3, 1, 1, bias=False)
 
     def forward(self, x):
         x = self.conv(x)
